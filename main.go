c567c185e8c746aad380362394402788efd10759
Michel Davalos <michel.davalos@academicos.udg.mx>
2021-12-27 10:30:53 -0600

    PrinterList struct to save Printers info, ServerInfo and DeviceInfo

--- main.go
+++ main.go
@@ -29,6 +29,20 @@ type Printer struct {
 	DStatus    string
 }
 
+type PrinterList struct {
+	Printers []Printer
+}
+
+func (p *PrinterList) FindPrinter(ip string) *Printer {
+	for i := range p.Printers {
+		if p.Printers[i].IP == ip {
+			//fmt.Printf("Address of i=%d:\t%p\n", printer, &printer)
+			return &p.Printers[i]
+		}
+	}
+	return nil
+}
+
 func GetIPs(fileName string) []string {
 	ips := []string{}
 
@@ -57,10 +71,20 @@ func main() {
 	ips := GetIPs("ip.json")
 	log.Println("IP list", ips)
 
-	var printerList []Printer
+	var printerList PrinterList
+
+	for _, ip := range ips {
+		printer := Printer{IP: ip}
+		printerList.Printers = append(printerList.Printers, printer)
+	}
 
 	// Instantiate default collector
 	c := colly.NewCollector(
+		// Visit only domains: hackerspaces.org, wiki.hackerspaces.org
+		colly.AllowedDomains(ips...),
+	)
+
+	c01 := colly.NewCollector(
 		// Visit only domains: hackerspaces.org, wiki.hackerspaces.org
 		colly.AllowedDomains(ips...),
 	)
@@ -71,12 +95,13 @@ func main() {
 	})
 
 	c.OnResponse(func(r *colly.Response) {
-		printer := Printer{}
-		printer.IP = r.Request.URL.Host
 		url := r.Request.URL.String()
 		//log.Println("Visited", url)
 		body := string(r.Body)
 		lines := strings.Split(body, "\n")
+
+		printer := printerList.FindPrinter(r.Request.URL.Host)
+		//fmt.Printf("Address of p=&i=%d:\t%p\n", *printer, printer)
 
 		if url == "http://"+r.Request.URL.Host+"/ServerInfo31.js" {
 			for i := 0; i < len(lines); i++ {
@@ -90,7 +115,23 @@ func main() {
 					break
 				}
 			}
-		} else if url == "http://"+r.Request.URL.Host+"/DeviceInfo32.js" {
+		}
+		//log.Println(printer)
+		//printerList = append(printerList, printer)
+		wg.Done()
+	})
+
+	c01.OnResponse(func(r *colly.Response) {
+		url := r.Request.URL.String()
+		//ip := r.Request.URL.Host
+		//log.Println("Visited", url)
+		body := string(r.Body)
+		//fmt.Println(body)
+		lines := strings.Split(body, "\n")
+
+		printer := printerList.FindPrinter(r.Request.URL.Host)
+
+		if url == "http://"+r.Request.URL.Host+"/DeviceInfo32.js" {
 			// fmt.Println(len(lines))
 			for i := 0; i < len(lines); i++ {
 				if strings.Contains(lines[i], "d1.model =") {
@@ -114,15 +155,13 @@ func main() {
 					// fmt.Println(len(code))
 					if len(code) >= 2 {
 						dstatus := strings.TrimSpace(code[1])
-						dstatus = strings.Trim(dstatus, "'")
-						printer.DStatus = strings.TrimSuffix(dstatus, "';//paper empty, error, ready")
+						dstatus = strings.Trim(dstatus, "''")
+						printer.DStatus = strings.TrimSuffix(dstatus, ";//paper empty, error, ready")
 						//log.Println(dstatus)
 					}
 				}
 			}
 		}
-		log.Println(printer)
-		printerList = append(printerList, printer)
 		wg.Done()
 	})
 
@@ -132,14 +171,22 @@ func main() {
 		wg.Done()
 	})
 
+	c01.OnError(func(r *colly.Response, err error) {
+		fmt.Println("Request URL:", r.Request.URL, "failed with response:", r, "\nError:", err)
+		wg.Done()
+	})
+
 	for _, ip := range ips {
 		link := "http://" + ip + "/ServerInfo31.js"
 		wg.Add(1)
 		go c.Visit(link)
 	}
+	wg.Wait()
+
+	wg.Add(1)
+	go c01.Visit("http://10.0.3.75/DeviceInfo32.js")
 
 	wg.Wait()
 
-	//c.Visit("http://10.0.3.75/DeviceInfo32.js")
-
+	fmt.Println(printerList)
 }
